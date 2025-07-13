ef103839ef2025ffa849205eefc9dbe1d5c8901e
Tackling <129611072+Tackling@users.noreply.github.com>
2025-07-13 18:31:48 +1000

    Update teamInfoQuery.js

--- gql/twitch/teamInfoQuery.js
+++ gql/twitch/teamInfoQuery.js
@@ -1,39 +1,59 @@
-module.exports = function getTeamInfoQuery(teamName) {
+module.exports = function getTeamInfoQuery(teamName, afterMembers = null, afterLive = null) {
   return [
     {
-      operationName: 'TeamLandingMemberList',
-      variables: { teamName },
+      operationName: 'TeamFullInfo',
+      variables: {
+        teamName,
+        afterMembers,
+        afterLive
+      },
       query: `
-        query TeamLandingMemberList($teamName: String!) {
+        query TeamFullInfo($teamName: String!, $afterMembers: Cursor, $afterLive: Cursor) {
           team(name: $teamName) {
-            members(first: 100) {
-              totalCount
-              edges {
-                node {
-                  login
-                  displayName
-                  id
-                }
-              }
-            }
-            owner {
-              login
-              displayName
-              id
-            }
-          }
-        }
-      `,
-    },
-    {
-      operationName: 'TeamsLandingBody',
-      variables: { teamName },
-      query: `
-        query TeamsLandingBody($teamName: String!) {
-          team(name: $teamName) {
+            id
             name
             displayName
-            id
+            description
+            backgroundImageID
+            backgroundImageURL
+            bannerID
+            bannerURL
+            logoID
+            logoURL
+            owner {
+              id
+              login
+              displayName
+            }
+            members(first: 100, after: $afterMembers) {
+              totalCount
+              edges {
+                cursor
+                node {
+                  id
+                  login
+                  displayName
+                }
+              }
+              pageInfo {
+                hasNextPage
+                endCursor
+              }
+            }
+            liveMembers(first: 100, after: $afterLive) {
+              edges {
+                cursor
+                node {
+                  id
+                  login
+                  displayName
+                }
+              }
+              pageInfo {
+                hasNextPage
+                endCursor
+              }
+            }
           }
         }
       `,

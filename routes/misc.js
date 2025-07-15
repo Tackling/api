9cfa972156c1b7870b2076b38d80feff93dd8158
Tackling <129611072+Tackling@users.noreply.github.com>
2025-07-15 23:09:09 +1000

    Update misc.js

--- routes/misc.js
+++ routes/misc.js
@@ -1,5 +1,27 @@
 const express = require('express');
+const humanizeDuration = require('humanize-duration');
 const router = express.Router();
+
+const humanizer = humanizeDuration.humanizer({
+  largest: 3,
+  units: ['y', 'mo', 'w', 'd', 'h', 'm', 's'],
+  round: true,
+  spacer: '',
+  delimiter: ', ',
+  conjunction: ' & ',
+  language: 'shortEn',
+  languages: {
+    shortEn: {
+      y: () => 'y',
+      mo: () => 'mo',
+      w: () => 'w',
+      d: () => 'd',
+      h: () => 'h',
+      m: () => 'm',
+      s: () => 's'
+    }
+  }
+});
 
 router.get('/misc', async (req, res) => {
   const start = Date.now();
@@ -9,12 +31,13 @@ router.get('/misc', async (req, res) => {
   const memoryUsage = process.memoryUsage();
   const heapUsedMB = (memoryUsage.heapUsed / 1024 / 1024).toFixed(2);
   const rssMB = (memoryUsage.rss / 1024 / 1024).toFixed(2);
+  const uptime = humanizer(process.uptime().toFixed(0) * 1000);
 
   res.json({
     ping: `${ping} ms`,
     heapUsedMB: `${heapUsedMB} MB`,
     rssMB: `${rssMB} MB`,
-    uptime: `${process.uptime().toFixed(2)} seconds`,
+    uptime
   });
 });
 

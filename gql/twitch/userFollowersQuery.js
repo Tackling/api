1892bbf478caa84ebd6230198fe3025e0813bb49
Tackling <129611072+Tackling@users.noreply.github.com>
2025-07-18 22:12:47 +1000

    Update userFollowersQuery.js

--- gql/twitch/userFollowersQuery.js
+++ gql/twitch/userFollowersQuery.js
@@ -2,7 +2,7 @@ module.exports = function getUserFollowersQuery(login, after = null) {
   return {
     query: `
       query Followers($login: String!, $after: Cursor) {
-        user(login: $login) {
+        user(login: $login, lookupType: ALL) {
           followers(first: 100, after: $after, order: DESC) {
             totalCount
             edges {
